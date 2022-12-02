f98223fcb5838d488f2281efdc7c456988c19e4c
tay cavett <taycavett98@gmail.com>
2022-12-02 07:56:42 -0600

    added controller class demo'd springApplication context managing my object

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -2,12 +2,24 @@ package guru.springframework.sfgdi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import guru.springframework.sfgdi.controllers.MyController;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
+
+		// get handle on spring application context
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+		// ask context for instance of controller
+		MyController myController = (MyController)ctx.getBean("myController");
+
+		String greetings = myController.sayHello();
+
+		System.out.println(greetings);
+
 	}
 
 }
