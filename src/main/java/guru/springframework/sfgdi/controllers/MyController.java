2730f4346d7c1d7409ec7e3f06c7259cce0254ac
tay cavett <taycavett98@gmail.com>
2022-12-02 09:10:09 -0600

    added ConstructorInjectedController added PropertyInjectedController added SetterInjectedController added service package for all controllers added qualifier examples added primary bean example

--- src/main/java/guru/springframework/sfgdi/controllers/MyController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/MyController.java
@@ -2,12 +2,18 @@ package guru.springframework.sfgdi.controllers;
 
 import org.springframework.stereotype.Controller;
 
+import guru.springframework.sfgdi.services.GreetingService;
+
 @Controller
 public class MyController {
     
-    public String sayHello(){
-        System.out.println("Hello World!");
+    private final GreetingService greetingService;
 
-        return "good morning";
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+    
+    public String sayHello(){
+        return greetingService.sayGreeting();
     }
 }
